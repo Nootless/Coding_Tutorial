b65375208294d086230dd178665d9f5168541309
Nootless <justin.zhang0403@gmail.com>
2022-10-18 12:20:21 -0400

    Added template for Project 1

---
+++ Lesson2/programs/calculator.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == '__main__':
+    # print(message)
+    # used to print a 
+    # input(message)
+    # used to take an input from the user
+    
+    # should follow a similar order to this
+    print('First Number:')
+    input() 
